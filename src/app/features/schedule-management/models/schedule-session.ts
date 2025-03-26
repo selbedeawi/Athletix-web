8c787dc163f9dbbec9ecf09fb00b83b9c0d0e8ab
mohammed-z-elrais <mohammedzelrais0@gmail.com>
2025-03-26 06:06:42 +0200

    fix for build

--- src/app/features/schedule-management/models/schedule-session.ts
+++ src/app/features/schedule-management/models/schedule-session.ts
@@ -3,22 +3,22 @@ import { Sessions } from "../../sessions-list/models/sessions";
 import { StaffAccount } from "../../staff-list/models/staff";
 
 export class ScheduleSession implements Tables<"ScheduledSession"> {
-    createdAt!: string;
-    createdBy!: string | null;
-    startTime!: string | null;
-    endTime!: string | null;
-    id!: string;
-    isPrivate!: boolean;
-    memberId!: string | null;
-    scheduledDate!: string | null;
-    sessionId!: string;
-    branchId!: string | null;
-    
-    // FE only
-    Sessions!:Sessions
-    coachIds!: string[];
-     SheduleCoaches?: {
+  createdAt!: string;
+  createdBy!: string | null;
+  startTime!: string | null;
+  endTime!: string | null;
+  id!: string;
+  isPrivate!: boolean;
+  memberId!: string | null;
+  scheduledDate!: string | null;
+  sessionId!: string;
+  branchId!: string | null;
+
+  // FE only
+  Sessions!: Sessions;
+  coachIds!: string[];
+  SheduleCoaches?: {
     Staff: StaffAccount;
     slotId: string;
-     }
+  };
 }
